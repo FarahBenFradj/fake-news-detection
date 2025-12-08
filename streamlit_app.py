13e0169381a57e850ba40f2d076a8f198be33060
FarahBenFradj <farah.benfradj@itbs.tn>
2025-12-08 22:25:33 +0100

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ except LookupError:
 # Load model and vectorizer
 @st.cache_resource
 def load_model():
-    with open('model/logistic_model.pkl', 'rb') as f:
+    with open('model/best_logistic_regression_model.pkl', 'rb') as f:
         model = pickle.load(f)
     with open('model/tfidf_vectorizer.pkl', 'rb') as f:
         vectorizer = pickle.load(f)
